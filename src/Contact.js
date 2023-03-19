f0d8cec2aeb51aa83889037be4ff451a0d25f04a
Zak-2005 <zak.mob2005@gmail.com>
2023-03-19 18:44:10 -0500

    style Contact Us page

--- src/Contact.js
+++ src/Contact.js
@@ -1,23 +1,34 @@
 export default function contactPage() {
     const content = document.querySelector("#content");
     const contact = document.createElement("div");
-  
+
+    const contactContent = document.createElement('div');
+    contactContent.classList.add('contactContent');
+
+    contact.classList.add('contactPage')
     const body = document.querySelector('body');
     body.style.backgroundImage = "url(../Pictures/woodBackground.jpg)"
+
 
     const header = document.createElement("div");
     header.textContent = "Contact Us";
     header.classList.add('heading');
     contact.appendChild(header);
+
+    const subhead = document.createElement('div');
+    subhead.classList.add('subhead');
+    subhead.textContent = 'If you have any questions, concerns, or want to reserve a table, feel free to contact us at:';
+    contactContent.appendChild(subhead);
   
     const email = document.createElement("div");
-    email.textContent = 'you@example.com'
-    contact.appendChild(email);
+    email.textContent = 'Email: you@example.com'
+    contactContent.appendChild(email);
     
     const phone = document.createElement("div");
-    phone.textContent = '999-999-9999'
-    contact.appendChild(phone);
+    phone.textContent = 'Phone Number: 999-999-9999'
+    contactContent.appendChild(phone);
 
+    contact.appendChild(contactContent);
     content.appendChild(contact);
   }
   
